ae25aa99d8318885a546a23755ca550303a8df5b
Reasty Kim <reastykim@orthosensor.com>
2017-11-15 11:22:37 +0900

    Added the Models(CongratulatoryMoney, ReturnPresent) for TakePage

---
+++ CongratulatoryMoneyManagement/Models/CongratulatoryMoney.cs
@@ -0,0 +1,52 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CongratulatoryMoneyManagement.Models
+{
+    public class CongratulatoryMoney : ObservableObject
+    {
+        /// <summary>
+        /// 금액
+        /// </summary>
+        public double Money
+        {
+            get => money;
+            set => Set(ref money, value);
+        }
+        private double money;
+
+        /// <summary>
+        /// 주신 분
+        /// </summary>
+        public string Name
+        {
+            get => name;
+            set => Set(ref name, value);
+        }
+        private string name;
+
+        /// <summary>
+        /// 봉투 사진 경로
+        /// </summary>
+        public string EnvelopeImageUri
+        {
+            get => envelopeImageUri;
+            set => Set(ref envelopeImageUri, value);
+        }
+        private string envelopeImageUri;
+
+        /// <summary>
+        /// 답례품
+        /// </summary>
+        public ReturnPresent ReturnPresent
+        {
+            get => returnPresent;
+            set => Set(ref returnPresent, value);
+        }
+        private ReturnPresent returnPresent = new ReturnPresent();
+    }
+}
---
+++ CongratulatoryMoneyManagement/Models/ReturnPresent.cs
@@ -0,0 +1,50 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CongratulatoryMoneyManagement.Models
+{
+    /// <summary>
+    /// 답례품 종류
+    /// </summary>
+    public enum ReturnPresentType
+    {
+        /// <summary>
+        /// 식권
+        /// </summary>
+        MealTickets,
+        /// <summary>
+        /// 차비
+        /// </summary>
+        Fare
+    }
+
+    /// <summary>
+    /// 답례품
+    /// </summary>
+    public class ReturnPresent : ObservableObject
+    {
+        /// <summary>
+        /// 답례품 종류
+        /// </summary>
+        public ReturnPresentType Type
+        {
+            get => type;
+            set => Set(ref type, value);
+        }
+        private ReturnPresentType type;
+
+        /// <summary>
+        /// 답례품 수량
+        /// </summary>
+        public uint Quantity
+        {
+            get => quantity;
+            set => Set(ref quantity, value);
+        }
+        private uint quantity = 1;
+    }
+}
