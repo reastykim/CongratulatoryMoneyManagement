9b3e58907c6b8d1b5fdb9491589815a7a9beee01
Reasty Kim <reastykim@orthosensor.com>
2017-12-21 11:39:54 +0900

    Removed the asyncinitializer in DataContext

--- CongratulatoryMoneyManagement/Data/CongratulatoryMoneyContext.cs
+++ CongratulatoryMoneyManagement/Data/CongratulatoryMoneyContext.cs
@@ -15,15 +15,12 @@ namespace CongratulatoryMoneyManagement.Data
         const string OldMainDbFileName = "congratulatoryMoney.db";
         const string MainDbFileName = "new_congratulatoryMoney.db";
 
-        private static AsyncInitilizer<CongratulatoryMoneyContext> initializer = new AsyncInitilizer<CongratulatoryMoneyContext>();
-
         #region Properties
 
         public DbSet<CongratulatoryMoney> CongratulatoryMoney
         {
             get
             {
-                initializer.CheckInitialized();
                 return congratulatoryMoney;
             }
 
@@ -38,7 +35,6 @@ namespace CongratulatoryMoneyManagement.Data
         {
             get
             {
-                initializer.CheckInitialized();
                 return moneyOptions;
             }
 
@@ -53,7 +49,6 @@ namespace CongratulatoryMoneyManagement.Data
         {
             get
             {
-                initializer.CheckInitialized();
                 return returnPresents;
             }
 
@@ -68,7 +63,6 @@ namespace CongratulatoryMoneyManagement.Data
         {
             get
             {
-                initializer.CheckInitialized();
                 return spendings;
             }
 
@@ -81,43 +75,13 @@ namespace CongratulatoryMoneyManagement.Data
 
         #endregion
 
-        #region Constructors
-
-        static CongratulatoryMoneyContext()
-        {
-            initializer.InitializeWith(CheckForDatabase);
-        }
-
-        #endregion
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite($"Data Source={MainDbFileName}");
         }
 
-        private static async Task CheckForDatabase()
-        {
-            var data = Windows.Storage.ApplicationData.Current.LocalFolder;
-
-            var exists = await data.TryGetItemAsync(OldMainDbFileName);
-
-            if (exists != null) // Renamined db file name for 1.0.0 version app.
-            {
-                await exists.RenameAsync(OldMainDbFileName + ".bak");
-            }
-
-            // Commentted, Because the table was made by Migration code
-            //if (exists == null)
-            //{
-            //    var mainDbAssetPath = $"ms-appx:///Assets/{OldMainDbFileName}";
-            //    var file = await StorageFile.GetFileFromApplicationUriAsync(new Uri(mainDbAssetPath)).AsTask().ConfigureAwait(false);
-            //    var database = await file.CopyAsync(data).AsTask().ConfigureAwait(false);
-            //}
-        }
-
         public static void CheckMigrations()
         {
-            initializer.CheckInitialized();
             using (var db = new CongratulatoryMoneyContext())
             {
                 db.Database.Migrate();
